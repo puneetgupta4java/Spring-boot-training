afb5547d1038984c1a5438716f9d8e4e36ae10e0
Ashi Bindal <ashibindal0@gmail.com>
2021-01-17 12:53:56 +0530

    Unused or Commented Code is removed

--- src/main/java/com/example/demo/Request/UserRequestCheck.java
+++ src/main/java/com/example/demo/Request/UserRequestCheck.java
@@ -3,24 +3,18 @@ package com.example.demo.Request;
 
 import java.util.List;
 
-import org.json.Cookie;
+
 import org.json.JSONArray;
 import org.json.JSONObject;
 import org.springframework.stereotype.Component;
-
 import com.example.demo.model.User;
-import com.fasterxml.jackson.databind.ObjectMapper;
-//import play.libs.Json;
-//import com.google.gson.JsonParser;
+
 
 @Component
 public class UserRequestCheck {
 	public boolean isJsonValid(User user)
 	{
 		try {
-			/*
-			 * final ObjectMapper mapper = new ObjectMapper(); mapper.readTree(User);
-			 */
 			
 			new JSONObject(user);
 		     return true;
@@ -29,10 +23,7 @@ public class UserRequestCheck {
 		{
 			
 			return false;
-			/*
-			 * try { Cookie.toJSONObject(User); return true; } catch (Exception e2) {
-			 * System.out.println("Invalid JSON"); }
-			 */
+			
 		}
 	}
 	
