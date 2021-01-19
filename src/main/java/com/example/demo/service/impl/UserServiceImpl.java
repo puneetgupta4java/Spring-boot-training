cdf5325ef2e9d319182b3f5e39fbf5c29f78bd41
Ashi Bindal <ashibindal0@gmail.com>
2021-01-20 02:09:35 +0530

    Added Service Annot, Used Streams

--- src/main/java/com/example/demo/service/impl/UserServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/UserServiceImpl.java
@@ -1,14 +1,14 @@
 package com.example.demo.service.impl;
 
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Optional;
 import java.util.Set;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
 import org.springframework.util.CollectionUtils;
 
 import com.example.demo.exception.UserException;
@@ -17,13 +17,13 @@ import com.example.demo.repository.UserRepository;
 import com.example.demo.service.UserService;
 import com.fasterxml.jackson.core.JsonProcessingException;
 
-@Component
+@Service
 public class UserServiceImpl implements UserService {
 	
 	@Autowired
 	private UserRepository userRepository;
 	
-	public List<User> getsearchUserByName(String name)
+	public List<User> searchUserByName(String name)
 	{
 		//Handling Null
 				if(name == null || name.trim().isEmpty())
@@ -33,57 +33,54 @@ public class UserServiceImpl implements UserService {
 				List<User> users= userRepository.findByName(name);	
 				if(CollectionUtils.isEmpty(users))
 				{
-					throw new UserException("User name not found in Database");
+					throw new UserException("User name not found in Database",HttpStatus.NOT_FOUND);
 				}
 				return users;
 				
 	}
 	
 	
-	public Optional<User> getsearchUserById(String id) throws JsonProcessingException {
-		System.out.println(id);
+	public Optional<User> searchUserById(String id) throws JsonProcessingException {
+	
 		if(id == null || id.trim().isEmpty())
 		{
 			throw new UserException("ID cannot be null or empty"); 
 		}
-	    Optional<User> optionalUser = userRepository.findById(id);
 	    
-	    //Handled Null Value
-	    if(optionalUser.isPresent())
-	    {	
-	 		return optionalUser;
-	    }
-	    else
-	    {
-	    	throw new UserException("User Data not found in Database");
-	    }
+		Optional<User> optionalUser = userRepository.findById(id);
+	    
+		
+		  //Handled Null Value 
+		if(optionalUser.isPresent()) 
+		{
+			return optionalUser; 
+		}
+		
+		else 
+		{ 
+			throw new
+			UserException("User Data not found in Database",HttpStatus.NOT_FOUND);
+		}
+		 
 	}
 	
-	public List<User> getsearchbyProjectName(String project) throws JsonProcessingException {
+	public List<User> searchbyProjectName(String project) throws JsonProcessingException {
 		if(project == null || project.trim().isEmpty())
 		{
-			throw new UserException("Project Name cannot be null or empty"); 
+			throw new UserException("Project Name cannot be null or empty",HttpStatus.NO_CONTENT); 
 		}
 		List<User> users = userRepository.findByProject(project);
 		if(CollectionUtils.isEmpty(users))
 		{
-			throw new UserException("No Data found");
+			throw new UserException("No Data found",HttpStatus.NOT_FOUND);
 		}
 	    return users;
 	}
 	
-	public List<String> getsearchbyClickProject() throws JsonProcessingException {
+	public Set<String> searchbyClickProject() throws JsonProcessingException {
 		ArrayList<User> users = (ArrayList<User>) userRepository.findAll();
-		
-		  Set<String> projects = new HashSet<String>(); 
-		  for(User u : users) {
-		  System.out.println(u.getProject().getClass().getName());
-		  projects.add(u.getProject());
-		  }
 		  
-		 // List<User> list = new ArrayList<>(projects);
-		 
-		return projects.stream().collect(Collectors.toList());
+		return users.stream().map(u -> u.getProject()).collect(Collectors.toSet());
 	}
 	
 }
