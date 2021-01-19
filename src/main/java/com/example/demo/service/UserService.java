6086c69816a077cda0257f47198d0b563459decf
Ashi Bindal <ashibindal0@gmail.com>
2021-01-20 02:05:45 +0530

    Removed Service Annotations, Set Meaning Names

--- src/main/java/com/example/demo/service/UserService.java
+++ src/main/java/com/example/demo/service/UserService.java
@@ -3,22 +3,17 @@
 package com.example.demo.service;
 
 
-import java.util.ArrayList;
+
 import java.util.List;
 import java.util.Optional;
 import java.util.Set;
-
-import org.springframework.stereotype.Service;
 import com.example.demo.model.User;
 import com.fasterxml.jackson.core.JsonProcessingException;
 
-@Service
 public interface UserService {
 	
-	
-	//Removed Object Mapper
-	public List<User> getsearchUserByName(String name);
-	public Optional<User> getsearchUserById(String id) throws JsonProcessingException;	
-	public List<User> getsearchbyProjectName(String project) throws JsonProcessingException;
-	public List<String> getsearchbyClickProject() throws JsonProcessingException;
+	public List<User> searchUserByName(String name);
+	public Optional<User> searchUserById(String id) throws JsonProcessingException;	
+	public List<User> searchbyProjectName(String project) throws JsonProcessingException;
+	public Set<String> searchbyClickProject() throws JsonProcessingException;
 	}
