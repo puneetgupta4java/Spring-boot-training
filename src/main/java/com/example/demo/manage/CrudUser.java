53e0edce48497287139a950cb166173e16f0f2ac
Ashi Bindal <ashibindal0@gmail.com>
2021-01-20 01:42:13 +0530

    Added Assembler, Removed Response Body,Added consumes and Produces, Modified Throw

--- src/main/java/com/example/demo/manage/CrudUser.java
+++ src/main/java/com/example/demo/manage/CrudUser.java
@@ -1,11 +1,9 @@
 package com.example.demo.manage;
 
 import java.util.List;
-//import java.util.Optional;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,48 +13,51 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.demo.assembler.UpdateUserAssembler;
 import com.example.demo.assembler.UserAssembler;
 import com.example.demo.model.User;
+import com.example.demo.response.dto.UserDto;
 import com.example.demo.response.dto.UserResponseDto;
 import com.example.demo.service.CrudService;
 
 
-//Create/Update/Delete User by ID
+//Added Code to perform CRUD operations
 
-//@SuppressWarnings("all")
 @RestController
-@RequestMapping(value= {"Crud","crud","CRUD"})
+@RequestMapping(value= {"crud"})
 public class CrudUser  {
 	
-	@Autowired
+	@Autowired 
 	private CrudService crudService;
 	
-	/*
-	 * @Autowired private UserAssembler userAssembler;
-	 */
+	@Autowired
+	private UpdateUserAssembler updateUserAssembler;
 	
-	@PutMapping(value={"update","change","set"})
-	public ResponseEntity<User> saveorUpdateUser(@RequestBody User user)
+	@Autowired
+	private UserAssembler userAssembler;
+	
+	//One DataSet will be updated at a Time
+	@PutMapping(value={"update"},consumes= MediaType.APPLICATION_JSON_VALUE,produces = { "application/json" })
+	public ResponseEntity<UserDto> saveorUpdateUser(@RequestBody User user)
 	{
-		User usr = crudService.getsaveorUpdateUser(user);
-		//UserResponseDto response = userAssembler.toModel(null, usr);
-		HttpHeaders responseHeaders = new HttpHeaders(); 
-		return new ResponseEntity<>(usr,responseHeaders,HttpStatus.ACCEPTED) ;
+		User usr = crudService.saveOrUpdateUser(user);
+		UserDto response = updateUserAssembler.toModel(usr);
+		return new ResponseEntity<>(response,HttpStatus.ACCEPTED) ;
 	}
 	
-	@PostMapping(value={"add","save"})
-	public ResponseEntity<List<User>> addUser(@RequestBody List<User> user)
+	
+	@PostMapping(value={"add"},consumes= MediaType.APPLICATION_JSON_VALUE,produces = { "application/json" })
+	public ResponseEntity<UserResponseDto> addUser(@RequestBody List<User> user)
 	{
-		List<User> users = crudService.getaddUser(user);
-		HttpHeaders responseHeaders = new HttpHeaders(); 
-		return new ResponseEntity<>(users,responseHeaders,HttpStatus.ACCEPTED);
+		List<User> users = crudService.addUser(user);
+		UserResponseDto response = userAssembler.toModel(users);
+		return new ResponseEntity<>(response,HttpStatus.ACCEPTED);
 	}
 	
-	@DeleteMapping(value= {"/delete/{id}","/remove{id}"})
+	@DeleteMapping(value= {"/delete/{id}"})
 	public ResponseEntity<String> deleteUser(@PathVariable("id") String id)
 	{
-		String s= crudService.getdeleteUser(id);
-		HttpHeaders responseHeaders = new HttpHeaders();
-		return new ResponseEntity<>(s,responseHeaders,HttpStatus.ACCEPTED);
+		String s= crudService.deleteUser(id);
+		return new ResponseEntity<>(s,HttpStatus.ACCEPTED);
 	}
 }
