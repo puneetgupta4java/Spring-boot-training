2ba4a0e2b0aaeeebeeaa50cfd8836eaa1009dd52
Ashi Bindal <ashibindal0@gmail.com>
2021-01-20 01:14:15 +0530

    Class for String Constants

---
+++ src/main/java/com/example/demo/commons/Constants.java
@@ -0,0 +1,6 @@
+package com.example.demo.commons;
+
+public class Constants {
+	public static final String JSON_STRING= "{\n\t\"id\": 123456\",\n\t\"name\": \"Ashi\"\n\t}";
+	public static final String JSON_ARR_STR= "[{\"id\" : \"123456\",\"Name\" : \"Ashi\"},{\"id\" : \"345678\", \"Name\" : \"Andrew\"}]";
+}
