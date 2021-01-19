67a144f9797a18eadeea18b3c37366647ed40ff7
Ashi Bindal <ashibindal0@gmail.com>
2021-01-20 01:55:00 +0530

    Added HttpStatus

--- src/main/java/com/example/demo/exception/UserException.java
+++ src/main/java/com/example/demo/exception/UserException.java
@@ -1,20 +1,35 @@
 package com.example.demo.exception;
 
+import org.springframework.http.HttpStatus;
 
 @SuppressWarnings({ "serial" })
 public class UserException extends RuntimeException{
 	String message;
-
+	HttpStatus httpStatus;
 	
 	public UserException() {
 		super();
 		// TODO Auto-generated constructor stub
 	}
 
+	public HttpStatus getHttpStatus() {
+		return httpStatus;
+	}
+
+	public void setHttpStatus(HttpStatus httpStatus) {
+		this.httpStatus = httpStatus;
+	}
+
 	public UserException(String message) {
 		this.message = message;
 	}
 	
+	
+	public UserException(String message, HttpStatus httpStatus) {
+		this.message = message;
+		this.httpStatus = httpStatus;
+	}
+
 	public String getMessage()
 	{
 		return message;
