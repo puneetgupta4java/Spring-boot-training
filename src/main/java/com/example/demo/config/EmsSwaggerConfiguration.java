0cd40d752fc163fcc0a1975f4c8e8d6cc3ec6881
Ashi Bindal <ashibindal0@gmail.com>
2021-01-20 02:11:36 +0530

    Customize Swagger

--- src/main/java/com/example/demo/config/EmsSwaggerConfiguration.java
+++ src/main/java/com/example/demo/config/EmsSwaggerConfiguration.java
@@ -1,10 +1,13 @@
 package com.example.demo.config;
 
+import java.util.Collections;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -15,6 +18,11 @@ public class EmsSwaggerConfiguration {
 	@Bean
 	public Docket api() {
 		return new Docket(DocumentationType.SWAGGER_2).select().apis(RequestHandlerSelectors.any())
-				.paths(PathSelectors.any()).build();
+				.paths(PathSelectors.any()).build().apiInfo(apiDetails());
+	}
+	
+	private ApiInfo apiDetails() {
+		return new ApiInfo("UserApi", "Sample Upi for Practice", "1.0",null, null, null, null,Collections.emptyList());
+
 	}
 }
